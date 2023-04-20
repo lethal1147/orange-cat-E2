2f0289724b4165e28314d69720ba6ea33dc13afc
Pimlanon <nonlaphan.pim@gmail.com>
2023-04-20 23:41:47 +0700

    style: add white container bg

--- client/src/pages/CreateCard/CreateCard.jsx
+++ client/src/pages/CreateCard/CreateCard.jsx
@@ -4,10 +4,13 @@ import Layout from "../../component/layout/layout"
 function CreateCard () {
     return (
         <Layout>
-            <div className='head-sentence'>
-                <h1 className='firsttopic'>Create Your Awesome Card</h1>
-                <h2 className='secondtopic'>Did You Meow Today</h2>
-                <h3 className='thirdtopic'>Today's Workout</h3>
+            <div className='container'>
+                <div className='head-sentence'>
+                    <h1 className='firsttopic'>Create Your Awesome Card</h1>
+                    <h2 className='secondtopic'>Did You Meow Today</h2>
+                    <h3 className='thirdtopic'>Today's Workout</h3>
+                </div>
+                
             </div>
         </Layout>
     )
