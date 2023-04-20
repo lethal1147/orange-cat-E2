461e81068ffaea819d96c5f70fed750d4316b6c3
Pimlanon <nonlaphan.pim@gmail.com>
2023-04-20 23:57:42 +0700

    style: add bg in createcard page

--- client/src/pages/CreateCard/CreateCard.jsx
+++ client/src/pages/CreateCard/CreateCard.jsx
@@ -4,13 +4,17 @@ import Layout from "../../component/layout/layout"
 function CreateCard () {
     return (
         <Layout>
+        <div className='bg'>
+
             <div className='container'>
                 <div className='head-sentence'>
                     <h1 className='firsttopic'>Create Your Awesome Card</h1>
                     <h2 className='secondtopic'>Did You Meow Today</h2>
                     <h3 className='thirdtopic'>Today's Workout</h3>
-                </div>
-                
+            </div>
+        </div>
+
+
             </div>
         </Layout>
     )
