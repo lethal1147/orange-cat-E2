930e107fafeb3afe2367ea9c3c082e3ff69a2689
Pimlanon <nonlaphan.pim@gmail.com>
2023-04-20 22:50:43 +0700

    initial commit

---
+++ client/src/pages/CreateCard/CreateCard.jsx
@@ -0,0 +1,11 @@
+import './CreateCard.css'
+
+function CreateCard () {
+    return (
+        <div className='createcard'>
+            <h1>Create card</h1>
+        </div>
+    )
+}
+
+export default CreateCard
