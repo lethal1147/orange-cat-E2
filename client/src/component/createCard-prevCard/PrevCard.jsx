d9f458bc5b30445b4b15a4bc4c2d9723d7bc45fb
Pimlanon <nonlaphan.pim@gmail.com>
2023-04-22 00:35:09 +0700

    add: add text mock up  in preview card

--- client/src/component/createCard-prevCard/PrevCard.jsx
+++ client/src/component/createCard-prevCard/PrevCard.jsx
@@ -11,13 +11,23 @@ function PrevCard() {
             <div className='prevImg'></div>
 
             <div className='container-text'>
-                <div className='prevTextbox'></div>
+                <div className='prevTextbox'>
+                    <p className='title-text'>This is title</p>
+                </div>
                 <FontAwesomeIcon icon={faPenToSquare} className='faPenToSquare' />
                 <FontAwesomeIcon icon={faTrashCan} className='faTrashCan' />
-                <div className='prevCaption'></div>
-                <div className='prevDate'></div>
+                <div className='prevCaption'>
+                    <p className='caption-text'>Dear diary today I found an  orange cat while running. but, that's not enough. 
+                    I still found a little black kitty. Luckily, I had pet's snack in my bag, so I fed them with my yummy yummy things.
+                    Actually, I just wanted to do some exercise, but I adopted 2 cats instead. </p>
+                </div>
+                <div className='prevDate'>
+                    <p className='date-text'>05 March 2023</p>
+                </div>
                 <FontAwesomeIcon icon={faCalendar} className='faCalendar' />
-                <div className='prevDuration'></div>
+                <div className='prevDuration'>
+                    <p className='duration-text'>1h 30m</p>
+                </div>
             </div>
 
             <div className='previewStatus'></div>
