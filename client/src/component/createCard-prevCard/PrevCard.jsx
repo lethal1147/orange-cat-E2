94b42a1c9b3bce74202ee07e98d60460cd45bb9c
Pimlanon <nonlaphan.pim@gmail.com>
2023-04-21 23:52:44 +0700

    style : add icon in card

--- client/src/component/createCard-prevCard/PrevCard.jsx
+++ client/src/component/createCard-prevCard/PrevCard.jsx
@@ -1,6 +1,8 @@
 import './PrevCard.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar } from '@fortawesome/free-regular-svg-icons'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
 
 function PrevCard() {
 
@@ -10,9 +12,11 @@ function PrevCard() {
 
             <div className='container-text'>
                 <div className='prevTextbox'></div>
+                <FontAwesomeIcon icon={faPenToSquare} className='faPenToSquare' />
+                <FontAwesomeIcon icon={faTrashCan} className='faTrashCan' />
                 <div className='prevCaption'></div>
                 <div className='prevDate'></div>
-                <FontAwesomeIcon icon={faCalendar} className='icon' />
+                <FontAwesomeIcon icon={faCalendar} className='faCalendar' />
                 <div className='prevDuration'></div>
             </div>
 
