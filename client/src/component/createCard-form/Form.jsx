ce246d1c75c25bbb895428b39fa5201b22b6e9d7
Pimlanon <nonlaphan.pim@gmail.com>
2023-04-22 14:46:55 +0700

    add: task status input

--- client/src/component/createCard-form/Form.jsx
+++ client/src/component/createCard-form/Form.jsx
@@ -21,51 +21,71 @@ function Form() {
                 <p>Activity:</p>
                 <div className='activity-list'>
                     <div className="tile"> 
-                        <input type="radio" value="biking" name="type" id='biking' ></input>
+                        <input type="radio" value="biking" name="type" id='biking'></input>
                         <label htmlFor="biking" className='activity-label'>
                             <img src={biking} alt='biking' className='activity-image'/>
                             <h6>Biking</h6>
                         </label>
                     </div>
                     <div className="tile"> 
-                        <input type="radio" value="running" name="type" id='running'  ></input>
+                        <input type="radio" value="running" name="type" id='running'></input>
                         <label htmlFor="running" className='activity-label'>
                             <img src={running} alt='running' className='activity-image'/>
                             <h6>running</h6>
                         </label>
                     </div>
                     <div className="tile"> 
-                        <input type="radio" value="swimming" name="type" id='swimming'  ></input>
+                        <input type="radio" value="swimming" name="type" id='swimming'></input>
                         <label htmlFor="swimming" className='activity-label'>
                             <img src={swimming} alt='swimming' className='activity-image'/>
                             <h6>swimming</h6>
                         </label>
                     </div>
                     <div className="tile"> 
-                        <input type="radio" value="cardio" name="type" id='cardio'  ></input>
+                        <input type="radio" value="cardio" name="type" id='cardio'></input>
                         <label htmlFor="cardio" className='activity-label'>
                             <img src={cardio} alt='cardio' className='activity-image'/>
                             <h6>cardio</h6>
                         </label>
                     </div>
                     <div className="tile"> 
-                        <input type="radio" value="walking" name="type" id='walking' ></input>
+                        <input type="radio" value="walking" name="type" id='walking'></input>
                         <label htmlFor="walking" className='activity-label'>
                         <img src={walking} alt='walking' className='activity-image'/>
                             <h6>walking</h6>
                         </label>
                     </div>
-
-                
-                
-                
-                
-                
-                
-                
                 </div>
             </div>
-            
+            {/* Add data: Duration */}
+            <div className='time_duration'>
+                <label htmlFor='time_start'>Time-Start:</label>
+                <input type='time' className='time1'></input>
+                <label htmlFor='time-end'className='time2'>Time-End:</label>
+                <input type='time' className='time2'></input>
+                <button>Duration</button>
+            </div>
+            {/* Add data: Task status */}
+            <div className='taskStatus'>
+                <p>Task Status:</p>
+                <div className='wrapper'>
+                    <input type='radio' value='green' name='task' id='complete'></input>
+                    <input type='radio' value='yellow' name='task' id='inProgress'></input>
+                    <input type='radio' value='red' name='task' id='fail'></input>
+                    <label htmlFor='complete' className='option option-1'>
+                        <div className='dot dot-1'></div>
+                        <span>Missison Complete</span>
+                    </label>
+                    <label htmlFor='inProgress' className='option option-2'>
+                        <div className='dot dot-2'></div>
+                        <span>In Progress</span>
+                    </label>
+                    <label htmlFor='fail' className='option option-3'>
+                        <div className='dot dot-3'></div>
+                        <span>Missison Failed</span>
+                    </label>
+                </div>
+            </div>            
 
             
         </form>
