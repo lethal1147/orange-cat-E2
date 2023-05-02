e7c51c9b07a65293af3abecab4eeb7d973e0ec63
Pimlanon <nonlaphan.pim@gmail.com>
2023-05-02 23:31:53 +0700

    ReadCard page set-up

---
+++ client/src/pages/ReadCard/ReadCard.jsx
@@ -0,0 +1,13 @@
+import './ReadCard.css';
+import Layout from "../../component/layout/layout"
+
+function ReadCard () {
+
+    return (
+        <Layout>
+            <h1>ReadCard</h1>
+        </Layout>
+    )
+}
+
+export default ReadCard;
