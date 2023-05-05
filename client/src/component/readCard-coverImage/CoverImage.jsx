51bfde243ffe249ac369db25d2f4c0c3a01ced72
Pimlanon <nonlaphan.pim@gmail.com>
2023-05-06 02:44:19 +0700

    completed: cover image

--- client/src/component/readCard-coverImage/CoverImage.jsx
+++ client/src/component/readCard-coverImage/CoverImage.jsx
@@ -1,12 +1,18 @@
 import './CoverImage.css';
 
-function CoverImage () {
+function CoverImage (props) {
+
+    const { image, getImage } = props;
 
     return (
-        <main>
-            <div>
-                <button className='btn-changecover'>Change cover</button>
-            </div>
+        <main className="cover-container">
+                <input type="file" accept="image/*" id="imgs" className="cover-input-field" name='image' 
+                    onChange={getImage} ></input>
+                <img src={image} className="cover-img-upload" />
+                <div className='btn-changecover'>
+                    <label className='click-here' htmlFor='imgs'>Change cover</label>
+                </div>
+            
         </main>
     )
 }
--- client/src/pages/ReadCard/ReadCard.jsx
+++ client/src/pages/ReadCard/ReadCard.jsx
@@ -1,15 +1,26 @@
 import './ReadCard.css';
+import { useState } from 'react'
 import Layout from "../../component/layout/layout";
 import CoverImage from "../../component/readCard-coverImage/CoverImage";
 import CallOut from "../../component/readCard-CallOut/CallOut";
 
 function ReadCard () {
+    const [image, setImage] = useState(null);
+    const [filename, setFilename] = useState('no selected file');
+
+    const getImage = ({ target: {files}}) => {
+        files[0] && setFilename(files[0].name)
+            if(files){
+                setImage(URL.createObjectURL(files[0]))
+            }
+    };
+
 
     return (
         <Layout>
             <main className='bg-readcard'>
                 <div className='r-coverimage'>
-                    <CoverImage />
+                    <CoverImage image={image} getImage={getImage} />
                 </div>
                 <div className='r-callout'>
                     <CallOut />
