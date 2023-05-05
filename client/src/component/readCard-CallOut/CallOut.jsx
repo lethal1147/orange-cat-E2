0784e51acd9d73a33cda711c23c6ab49fd514041
Pimlanon <nonlaphan.pim@gmail.com>
2023-05-06 00:23:00 +0700

    fix: layout

---
+++ client/src/component/readCard-CallOut/CallOut.jsx
@@ -0,0 +1,15 @@
+import './CallOut.css';
+
+function CallOut () {
+
+    return (
+        <main>
+            <div>
+                <textarea className='textarea'></textarea>
+            </div>
+
+        </main>
+    )
+}
+
+export default CallOut;
--- client/src/pages/ReadCard/ReadCard.jsx
+++ client/src/pages/ReadCard/ReadCard.jsx
@@ -1,17 +1,18 @@
 import './ReadCard.css';
-import Layout from "../../component/layout/layout"
-import CoverImage from "../../component/readCard-coverImage/CoverImage"
+import Layout from "../../component/layout/layout";
+import CoverImage from "../../component/readCard-coverImage/CoverImage";
+import CallOut from "../../component/readCard-CallOut/CallOut";
 
 function ReadCard () {
 
     return (
         <Layout>
             <main className='bg-readcard'>
-                <div className='cover-image'>
+                <div className='r-coverimage'>
                     <CoverImage />
                 </div>
-                <div className='quote'>
-                    
+                <div className='r-callout'>
+                    <CallOut />
                 </div>
             </main>
             
