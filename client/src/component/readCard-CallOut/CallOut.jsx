b12372cdb3ba8551b0cdb9632f96c5bd11a77d59
Pimlanon <nonlaphan.pim@gmail.com>
2023-05-07 02:40:21 +0700

    add: completed callout feature

--- client/src/component/readCard-CallOut/CallOut.jsx
+++ client/src/component/readCard-CallOut/CallOut.jsx
@@ -2,52 +2,49 @@ import './CallOut.css';
 import { useState } from 'react';
 import Picker from '@emoji-mart/react';
 import data from '@emoji-mart/data'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceSmile } from '@fortawesome/free-regular-svg-icons'
 function CallOut () {
 
-    const [pickerVisible, setPickerVisible] = useState(false);
-    const [currentEmoji, setCurrentEmoji] = useState(null);
+    const [ pickerVisible, setPickerVisible ] = useState(false);
+    const [ currentEmoji, setCurrentEmoji ] = useState(null);
+    const [ callOut, setCallout ] = useState('');
+
+    const showPicker = () => {
+        setPickerVisible(!pickerVisible);
+    }
+
+    const pickEmoji = (e) => {
+        setCurrentEmoji(e.native);
+        setPickerVisible(!pickerVisible);
+    }
+
+    const handleChangeCallOut = (e) => {
+        const { name, value } = e.target
+        //console.log({...callOut})
+        setCallout({...callOut,[name]: value})
+        // console.log({...callOut})
+    }
 
     return (
         <main className='emojiContainer'>
-            {/* <div className='emoji-subcontainer'>
-                <h1 className='yes'>{ currentEmoji || `Yes` }</h1>
-                <div className='emoji-box' onClick={() => setPickerVisible(!pickerVisible)}></div>
-                <div className={pickerVisible ? 'd-block' : 'd-none'}>
-                    <Picker 
-                        data={data} 
-                        previewPosition="none"
-                        onEmojiSelect = {(e) => {
-                            setCurrentEmoji(e.native);
-                            setPickerVisible(!pickerVisible);
-                        }}
-                    />
+            <div className='emoji-allcontainer'>
+                <div className='emoji-subcontainer'>
+                    <h3 className='displayEmoji'>{ currentEmoji }</h3> 
+                    
+                    <textarea className='textarea' name='callout' onChange={handleChangeCallOut} 
+                    placeholder='Got a quote that inspires you to keep pushing? tell us here! ' ></textarea>
                 </div>
-                <textarea className='textarea'></textarea>
-            </div> */}
-
-            
-                <h1 className='yes'>{ currentEmoji || `Yes` }</h1>
-                <button
-                    className='btn btn-primary'
-                    onClick={() => setPickerVisible(!pickerVisible)}
-                >
-                open emoji picker
-                </button>
-                <div style={{display: pickerVisible ? 'block' : 'none'}}>
-                    <Picker 
-                        data={data}
-                        previewPosition = 'none'
-                        onEmojiSelect = {(e) => {
-                            setCurrentEmoji(e.native);
-                            setPickerVisible(!pickerVisible);
-                        }}
-                    />
-
+                <FontAwesomeIcon icon={faFaceSmile} className='fa-facesmile' onClick={showPicker} />
+                <div style={{display: pickerVisible ? 'block' : 'none'}} className='emoji-picker' >
+                        <Picker 
+                            data={data}
+                            previewPosition = 'none'
+                            onEmojiSelect = {pickEmoji}
+                        />
                 </div>
-                <textarea className='textarea'></textarea>
-            
-
+            </div>
+            <div className='callout-line'></div>
         </main>
     )
 }
