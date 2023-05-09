2962b3e738fd7443706e10b9dbcdcb306ab5479c
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-09 09:19:37 +0700

    Dashboard page set-up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/Login/login';
 import Register from './pages/Register/register';
 import CreateCard from './pages/CreateCard/CreateCard';
 import ReadCard from './pages/ReadCard/ReadCard';
+import Dashboard from './pages/Dashboard/Dashboard';
 import { Routes, Route } from 'react-router-dom'
 import "./App.css"
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="createcard" element={<CreateCard />} />
+        <Route path="dashboard" element={<Dashboard />} />
         <Route path="readcard" element={<ReadCard />} />
       </Routes>
     </div>
