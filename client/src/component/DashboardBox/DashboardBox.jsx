906d55a9b0469c4b963f6b08e7c443ee312cc8cc
ArmSiwatK <arm.siwatk@gmail.com>
2023-05-09 09:19:37 +0700

    Dashboard Progress Card

--- client/src/component/DashboardBox/DashboardBox.jsx
+++ client/src/component/DashboardBox/DashboardBox.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './DashboardBox.css';
 import DashboardProfile from './DashboardComponents/DashboardProfile/dashboardProfile';
+import DashboardProgress from './DashboardComponents/DashboardProgress/DashboardProgress';
 
 export default function DashboardBox() {
     useEffect(() => {
@@ -12,8 +13,13 @@ export default function DashboardBox() {
 
     return (
         <div className="dashboard-container">
-            <div className="dashboard-card">
-            <DashboardProfile />
+            <div className="dashboard-column">
+                <div className="dashboard-card">
+                    <DashboardProfile />
+                </div>
+                <div className="dashboard-card">
+                    <DashboardProgress />
+                </div>
             </div>
         </div>
     );
