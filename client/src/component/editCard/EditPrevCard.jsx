a178eeb633312403664219e5c72822a6b2137eec
Pimlanon <nonlaphan.pim@gmail.com>
2023-05-11 14:10:33 +0700

    completed: eidt card

---
+++ client/src/component/editCard/EditPrevCard.jsx
@@ -0,0 +1,65 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendar, faPenToSquare, faTrashCan } from '@fortawesome/free-regular-svg-icons'
+import biking from '../../assets/images/CreateCard/biking.png';
+import running from '../../assets/images/CreateCard/running.png';
+import swimming from '../../assets/images/CreateCard/swimming.png';
+import cardio from '../../assets/images/CreateCard/cardio.png';
+import walking from '../../assets/images/CreateCard/walking.png';
+import Uploader from '../createCard-Uploader/Uploader'
+
+
+function EditPrevCard( {inputs, task, image, handleFileChange} ) {
+
+    
+
+    return (
+        <>
+            <div className='prevcard'>
+                {/* preview Image */}
+                <div className='prevImg'>
+                    <Uploader image={image} handleFileChange={handleFileChange} />
+                </div>
+                
+
+                {/* preview title */}
+                <div className='container-text'>
+                    <div className='prevTextbox'>
+                        <p className='title-text'>{inputs.title}</p>
+                    </div>
+                    {/* edit and delete icon */}
+                    <FontAwesomeIcon icon={faPenToSquare} className='faPenToSquare' />
+                    <FontAwesomeIcon icon={faTrashCan} className='faTrashCan' />
+
+                    {/* preview caption */}
+                    <div className='prevCaption'>
+                        <p className='caption-text'>{inputs.caption}</p>
+                    </div>
+
+                    {/* preview date */}
+                    <div className='prevDate'>
+                        <p className='date-text'>{inputs.date}</p>
+                    </div>
+                    {/* date icon */}
+                    <FontAwesomeIcon icon={faCalendar} className='faCalendar' />
+
+                    {/* preview duration */}
+                    <div className='prevDuration'>
+                        {inputs.type === 'biking' && <img src={biking} className='icon-img'  />} 
+                        {inputs.type === 'running' && <img src={running} className='icon-img'  />}
+                        {inputs.type === 'swimming' && <img src={swimming} className='icon-img-swimming'  />}    
+                        {inputs.type === 'cardio' && <img src={cardio} className='icon-img'  />}
+                        {inputs.type === 'walking' && <img src={walking} className='icon-img'  />}
+                        <p className='duration-text'>{inputs.duration}</p>
+                    </div>
+                </div>
+
+                {/* preview task status */}
+                <div className='previewStatus' style={{backgroundColor:task}}></div>
+            </div>
+
+            
+        </>
+    )
+}
+
+export default EditPrevCard
--- client/src/pages/EditCard/EditCard.jsx
+++ client/src/pages/EditCard/EditCard.jsx
@@ -1,33 +1,137 @@
-// import './EditCard.css';
-import { useState } from 'react';
-import Layout from "../../component/layout/layout"
-import SideContainer from '../../component/editCard-sideContainer/SideContainer'
-import PrevCard from "../../component/createCard-prevCard/PrevCard"
-import Form from '../../component/createCard-form/Form'
+import { useState } from "react";
+import Layout from "../../component/layout/layout";
+import SideContainer from "../../component/editCard-sideContainer/SideContainer";
+import EditPrevCard from "../../component/editCard/EditPrevCard";
+import EditForm from "../../component/editCard/EditForm"
 
-function EditCard () {
+function EditCard() {
+
+    const [ task, setTask ] = useState('');
+    const [ image, setImage ] = useState(null);
+    const [filename, setFilename] = useState('no selected file');
+    const [ inputs, setInputs] = useState ({
+        title: `wait for get user's data`,
+        caption: `wait for get user's data`,
+        time_start: '',
+        time_end: '',
+        duration: '',
+        date: '',
+        task: '',
+        type: '',
+        img: ''
+    })
 
 
-    return (
-        <Layout>    
-            <div className='create-card-container'>
-                <div className='bg'>
-                    <SideContainer />
-                    <div className='container'>
-                        <div className='head-sentence'>
-                            <h1 className='firsttopic'>Edit Your Awesome Card</h1>
-                            <h2 className='secondtopic'>Did You Meow Today?</h2>
-                            <h3 className='thirdtopic'>Today's Workout</h3>
-                        </div>
-                        {/* <PrevCard  />
-                        <Form  /> */}
-                        
-                    </div>
-                </div>
+    function calcDuration () {
+        let tStart = inputs.time_start;
+        let tEnd = inputs.time_end;
+        // console.log(tStart, tEnd);
+    
+        //convert timeStart and timeEnd to milliseconds
+        let milliseconds1 = Number(tStart.split(':')[0]) * 60 * 60 * 1000 + Number(tStart.split(':')[1]) * 60 * 1000;
+        let milliseconds2 = Number(tEnd.split(':')[0]) * 60 * 60 * 1000 + Number(tEnd.split(':')[1]) * 60 * 1000;
+        // console.log(milliseconds1, milliseconds2);
+    
+        //find differences in milliseconds
+        let difference = Math.abs(milliseconds2 - milliseconds1);
+        if (milliseconds2 < milliseconds1) {
+            difference = 86400000 - difference;
+        }
+        // console.log(difference)
+    
+        let msec = difference;
+                let hh = Math.floor(msec / 1000 / 60 / 60);
+                msec -= hh * 1000 * 60 * 60;
+                //console.log('hh ' + hh);
+                //console.log('hh ' + msec);
+                let mm = Math.floor(msec / 1000 / 60);
+                //console.log('mm1 ' + mm);
+                msec -= mm * 1000 * 60;
+                //console.log('mm2 ' + msec);
+                let ss = Math.floor(msec / 1000);
+                msec -= ss * 1000;
+    
+                // console.log(hh + ":" + mm + ":" + ss);
+    
+                //prepare to display in preview card
+                let displayHour = hh + ' h';
+                let displayMin = mm + ' m'
+                // console.log(displayHour, displayMin)
+    
+                let hour;
+                let min; 
+                if (hh === 0) {
+                    hour = '';
+                    min = displayMin;
+                } else if (mm === 0) {
+                    hour = displayHour;
+                    min = '';
+                } else if (hh > 0 && mm > 0) {
+                    hour = displayHour;
+                    min = displayMin;
+                } else {
+                    hour = '0 h';
+                    min = '0 m';
+                }
+                let calculateDuration = `${hour} ${min}`
+                setInputs({...inputs,duration:calculateDuration})
+        }
+
+        let changeColor = (e) => {
+            const color = ['#96d674', '#fff476', '#fd8888']
+            const { value } = e.target
+            if (value == 'complete') {
+                //console.log('you click green')
+                setTask(color[0])
+            }
+            if (value == 'inProgress') {
+                //console.log('you click yellow')
+                setTask(color[1])
+            }
+            if (value == 'fail') {
+                // console.log('you click red')
+                setTask(color[2])
+            }
+        }
+
+    const handleChangeInput = (e) => {
+        const { name, value } = e.target
+        //console.log({...inputs})
+        setInputs((prevInputs) => ({...prevInputs,[name]: value}));
+    }
+
+    console.log(inputs)
+
+    function handleFileChange (e) {
+        const { files } = e.target;
+        if (files && files[0]) {
+            const file = files[0];
+            setFilename(file.name);
+            setImage(URL.createObjectURL(file));
+            handleChangeInput({target: {name: 'img', value: file}});
+            }
+        }
+
+  return (
+    <Layout>
+      <div className="create-card-container">
+        <div className="bg">
+          <SideContainer />
+          <div className="container">
+            <div className="head-sentence">
+              <h1 className="firsttopic">Edit Your Awesome Card</h1>
+              <h2 className="secondtopic">Did You Meow Today?</h2>
+              <h3 className="thirdtopic">Today's Workout</h3>
             </div>
-        </Layout>
-        
-    )
+            {/* card */}
+            <EditPrevCard inputs={inputs} image={image} handleFileChange={handleFileChange} task={task} handleChangeInput={handleChangeInput} />
+            {/* form */}
+            <EditForm handleChangeInput={handleChangeInput} calcDuration={calcDuration} changeColor={changeColor} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export default EditCard
+export default EditCard;
